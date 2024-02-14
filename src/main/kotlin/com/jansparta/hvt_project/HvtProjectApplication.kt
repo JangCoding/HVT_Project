fadf01cadb7af046c19e94a48af6035939eedd6a
codingjang <jjangga813@naver.com>
2024-02-14 11:45:07 +0900

    Initial commit

---
+++ src/main/kotlin/com/jansparta/hvt_project/HvtProjectApplication.kt
@@ -0,0 +1,11 @@
+package com.jansparta.hvt_project
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class HvtProjectApplication
+
+fun main(args: Array<String>) {
+    runApplication<HvtProjectApplication>(*args)
+}
---
+++ src/test/kotlin/com/jansparta/hvt_project/HvtProjectApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.jansparta.hvt_project
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class HvtProjectApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
