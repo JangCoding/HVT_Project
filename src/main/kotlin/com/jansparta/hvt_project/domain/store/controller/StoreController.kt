e213d3b88aa71634856acc2c64ac68df4caebe98
JangCoding <62090021+JangCoding@users.noreply.github.com>
2024-02-15 12:30:29 +0900

    Revert "Store Controller, Service 메서드 생성, Store DTO 생성"

--- src/main/kotlin/com/jansparta/hvt_project/domain/store/controller/StoreController.kt
+++ src/main/kotlin/com/jansparta/hvt_project/domain/store/controller/StoreController.kt
@@ -1,15 +1,8 @@
 package com.jansparta.hvt_project.domain.store.controller
 
-import com.jansparta.hvt_project.domain.store.dto.CreateStoreRequest
-import com.jansparta.hvt_project.domain.store.dto.StoreResponse
-import com.jansparta.hvt_project.domain.store.dto.UpdateStoreRequest
 import com.jansparta.hvt_project.domain.store.service.StoreService
-import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.GetMapping
-import org.springframework.web.bind.annotation.PathVariable
 import org.springframework.web.bind.annotation.PostMapping
-import org.springframework.web.bind.annotation.PutMapping
-import org.springframework.web.bind.annotation.RequestBody
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
 
@@ -20,23 +13,6 @@ class StoreController (
 ){
     @PostMapping("/collection") // 업체 리스트 csv 불러오기
     fun getStoresFromCSV()
-    {
-        TODO()
-    }
-
-
-    @PostMapping("/create")
-    fun createStore(
-        @RequestBody request : CreateStoreRequest
-    ) : ResponseEntity<StoreResponse>
-    {
-        TODO()
-    }
-
-    @PutMapping("/update")
-    fun updateStore(
-        @RequestBody request : UpdateStoreRequest
-    ) : ResponseEntity<StoreResponse>
     {
         TODO()
     }
@@ -61,14 +37,6 @@ class StoreController (
 
     @GetMapping("/filtered/simple")
     fun getFilteredSimpleStore()
-    {
-        TODO()
-    }
-
-    @GetMapping("{id}")
-    fun getStoreById(
-        @PathVariable id : Long
-    ) : ResponseEntity<StoreResponse>
     {
         TODO()
     }
--- src/main/kotlin/com/jansparta/hvt_project/domain/store/repository/StoreRepository.kt
+++ src/main/kotlin/com/jansparta/hvt_project/domain/store/repository/StoreRepository.kt
@@ -1,8 +1,4 @@
 package com.jansparta.hvt_project.domain.store.repository
 
-import com.jansparta.hvt_project.domain.store.model.Store
-import org.springframework.data.jpa.repository.JpaRepository
-
-interface StoreRepository : JpaRepository<Store, Long> {
-
+interface StoreRepository {
 }
