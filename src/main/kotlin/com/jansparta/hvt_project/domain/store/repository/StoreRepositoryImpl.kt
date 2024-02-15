d10d47d0cb7ea117c392e4ef1cdb5b0120d46874
nohha0 <151031005+nohhayeong@users.noreply.github.com>
2024-02-15 16:19:39 +0900

    filter 적용 리스트 조회

--- src/main/kotlin/com/jansparta/hvt_project/domain/store/repository/StoreRepositoryImpl.kt
+++ src/main/kotlin/com/jansparta/hvt_project/domain/store/repository/StoreRepositoryImpl.kt
@@ -1,7 +1,9 @@
 package com.jansparta.hvt_project.domain.store.repository
 
 import com.jansparta.hvt_project.domain.store.model.QStore
+import com.jansparta.hvt_project.domain.store.model.Store
 import com.jansparta.hvt_project.infra.querydsl.QueryDslSupport
+import com.querydsl.core.BooleanBuilder
 import org.springframework.stereotype.Repository
 
 @Repository
@@ -10,4 +12,15 @@ class StoreRepositoryImpl : CustomStoreRepository, QueryDslSupport() {
 
     private val store = QStore.store
 
+    override fun findByRatingAndStatus(rating: Int?, status: String?): List<Store> {
+        val whereClause = BooleanBuilder()
+
+        rating?.let { whereClause.and(store.totRatingPoint.eq(it)) }
+        status?.let { whereClause.and(store.statNm.eq(it)) }
+
+        return queryFactory.selectFrom(store)
+            .where(whereClause)
+            .orderBy(store.regDate.desc())
+            .fetch()
+    }
 }
