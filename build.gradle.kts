fb25f142c95cfa3e6dda30c8944f17db4d5fcfc8
codingjang <jjangga813@naver.com>
2024-02-16 15:58:04 +0900

    테스트 의존성 추가, yml 설정

--- build.gradle.kts
+++ build.gradle.kts
@@ -78,6 +78,13 @@ dependencies {
     // AOP 패키지 설정
     implementation("org.springframework.boot:spring-boot-starter-aop")
 
+    // Test 관련 의존성 추가
+    runtimeOnly("com.h2database:h2")
+    testImplementation("org.springframework.boot:spring-boot-starter-test")
+    testImplementation("io.mockk:mockk:1.13.9")
+    testImplementation("io.kotest:kotest-runner-junit5:5.7.2")
+    testImplementation("io.kotest:kotest-assertions-core:5.7.2")
+
 }
 
 tasks.withType<KotlinCompile> {
